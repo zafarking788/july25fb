9a1bdd75231997e53d7fa58d22817ea0ae4ab843
CastaliaIkz <68614343+Kagurazaga@users.noreply.github.com>
2020-07-25 23:21:17 +0000

    Create logo.py

---
+++ lib/logo.py
@@ -0,0 +1,21 @@
+import os
+w = '\x1b[1;37m'
+b = '\x1b[1;36m'
+g = '\x1b[1;32m'
+y = '\x1b[1;33m'
+r = '\x1b[1;31m'
+
+def banner():
+  os.system('clear')
+  print(f'''{r} 
+     __            _     _ _         
+    / /__ __ _ ___| |_  (_) | __ ____
+   / / __/ _` / __| __| | | |/ /|_  /
+  / / (_| (_| \__ \ |_ _| |   <  / / 
+ /_/ \___\__,_|___/\__(_)_|_|\_\/___|
+                                     XamppGangstaTeam
+{r}====> >====> >====> >====> >====> >====> >====> >====>
+{r}║{y}*{b} Author{w} :{g} CastaliaIkz             
+{r}║{y}*{w} Coder{w}:{r} Kagurazaga                      
+{r}║{y}*{g} Facebook{w} :{b} \x1b[4mMoch Bagas Kurniawan 
+{r}====> >====> >====> >====> >====> >====> >====> >====>''')
